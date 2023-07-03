5b9daae445db7da16f58cbf97fd4eb8c0f7f9186
spogshia <138505637+spogshia@users.noreply.github.com>
2023-07-03 12:40:41 -0400

    Add files via upload

---
+++ deliverable1.cs
@@ -0,0 +1,59 @@
+﻿
+
+
+namespace Conditional_Example_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0;
+            string series = "";
+
+            while (true)
+            {
+                Console.Write("Enter an integer number between q and 100 ");
+                input = int.Parse(Console.ReadLine());
+
+                if (input >= 1 && input <= 100)
+                {
+                    break;
+
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Specify the series tye: Even or odd ");
+                    series = Console.ReadLine();
+
+                    if (series == "Even" || series == "Odd")
+                    {
+                        break;
+                    }
+                }
+
+                if (series == "Odd")
+                {
+                    Console.WriteLine("You have selected " + series + "series. The numbers between 0 and " + input + " are ");
+                    for (int i = 1; i < input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+                if (series == "Even")
+                {
+                    Console.WriteLine("You have selected " + series + "series. The numbers between 0 and " + input + " are ");
+                    for (int i = 0; i <= input; i = i + 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+
+            }
+        }
+    }
+
+}
+             
